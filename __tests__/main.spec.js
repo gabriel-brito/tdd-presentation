5c0838871971f43e341ed2399cc20ed330d25192
Gabriel <mfkrgabriel@gmail.com>
2018-05-07 09:56:26 -0300

    Finishing the calculator TDD

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -21,4 +21,43 @@ describe('Main', ()=> {
     });
   });
 
+  describe('Main Sum method test', ()=> {
+    it('should be equal 4 when sum(2,2)', ()=> {
+      expect(sum(2,2)).to.be.equal(4);
+    });
+
+    it('should be equal 8 when sum(4,4)', ()=>{
+      expect(sum(4,4)).to.be.equal(8);
+    });
+  });
+
+  describe('Main Sub method test', ()=> {
+    it('should be equal 0 when sub(2,2)', ()=> {
+      expect(sub(2,2)).to.be.equal(0);
+    });
+
+    it('should be equal 2 when sub(4,2)', ()=>{
+      expect(sub(4,2)).to.be.equal(2);
+    });
+  });
+
+  describe('Main mult method test', ()=> {
+    it('should be equal 4 when mult(2,2)', ()=> {
+      expect(mult(2,2)).to.be.equal(4);
+    });
+
+    it('should be equal 16 when mult(4,4)', ()=>{
+      expect(mult(4,4)).to.be.equal(16);
+    });
+  });
+
+  describe('Main div method test', ()=> {
+    it('should be equal 1 when div(2,2)', ()=> {
+      expect(div(2,2)).to.be.equal(1);
+    });
+
+    it('should be equal 2 when div(10,5)', ()=>{
+      expect(div(10,5)).to.be.equal(2);
+    });
+  });
 });
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
-const sum = () => {}
-const sub = () => {}
-const mult = () => {}
-const div = () => {}
+const sum = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const mult = (a, b) => a * b;
+
+const div = (a, b) => a / b;
 
 export {
   sum,
